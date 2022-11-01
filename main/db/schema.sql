ed4e74819e4df4d02625e54a2fa2f68047feacbe
KaptSprinkles <KSoto18@users.noreply.github.com>
2022-11-01 17:23:55 -0400

    role table

--- main/db/schema.sql
+++ main/db/schema.sql
@@ -5,7 +5,13 @@ USE employee_db;
 
 CREATE TABLE department (
     id INT NOT NULL AUTO_INCREMENT,
-    dept_name VARCHAR(25) NOT NULL,
+    dept_name VARCHAR(30) NOT NULL,
     PRIMARY KEY(id)
 );
 
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DEC(13, 2) NOT NULL,
+    dept_id INT
+)
