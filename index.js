57deba700499517ae552d0af0d3032c65dfca21b
KaptSprinkles <KSoto18@users.noreply.github.com>
2022-11-01 17:06:37 -0400

    created userOptions() with a switch for selection

--- index.js
+++ index.js
@@ -2,23 +2,48 @@ const inquirer = require('inquirer');
 
 
 
-const userOptions = () => {
-    inquirer.prompt ([
-            {
-             type: 'list',
-             name: 'options',
-             message: 'Select and option: ',
-             choices: [
+function userOptions() {
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'options',
+            message: 'Select and option: ',
+            choices: [
                 'View all Departments',
                 'View all Roles',
                 'View all Employees',
                 'Add a Department',
                 'Add a Role',
                 'Add an Employee',
-                'Update an Emplpoyee Role'
-             ]
-            }
-        ]
-    )
+                'Update an Emplpoyee Role',
+            ]
+        }
+    ])
+    .then(function(selection) {
+        switch(selection.options){
+            case 'View all Departments':
+                departments();
+            break;
+            case 'View all Roles':
+                roles();
+            break;
+            case 'View all Employees':
+                employees();
+            break;
+            case 'Add a Department':
+                addDepartment();
+            break;
+            case 'Add an Employee':
+                addEmployee();
+            break;
+            case 'Update an Emplpoyee Role':
+                updateEmployee();
+            break;
+
+            default:
+                ok();
+        }
+    })
 }
 
+userOptions();
