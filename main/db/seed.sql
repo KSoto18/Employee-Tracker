fd061666992dc7dfbaa9fd3306fd5d4dc8be5227
KaptSprinkles <KSoto18@users.noreply.github.com>
2022-11-01 18:16:27 -0400

    added roles and employees to seed

--- main/db/seed.sql
+++ main/db/seed.sql
@@ -5,3 +5,16 @@ VALUES ('Front Of House'),
        ('Marketing'),
        ('Catering');
 
+INSERT INTO role(title, salary, dept_id)
+VALUES ('Director', 65000, 1),
+       ('Manager', 40000, 2),
+       ('Supervisor', 32000, 3),
+       ('Team Member', 28000, 4);
+
+INSERT INTO employee(first_name, last_name, role_id, manager_id)
+VALUES ('Korben', 'Dallas', 1, null),
+       ('Raymon', 'Reddintong', 1, null),
+       ('Thomas', 'Anderson', 2, 1),
+       ('Arthur', 'Fleck', 3, 2),
+       ('Alfred', 'Pennyworth', 3, 1),
+       ('Bruce', 'Banner', 4, 2);
